7d8dff9760fc8898c568be3be82e35a4e5eba04b
Computhink-CS146 <cs146@computhink.com.sg>
2025-12-02 06:24:09 +0000

    Dec 2, 2025, 6:24 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -11,7 +11,7 @@ for count in range(100):
 # Task 2:
 name = "Jovan".upper()
 for letter in name:
-    print("give me a " + letter + ())
+    print("give me a " + letter + "!")
 
 
 ########################################################################
