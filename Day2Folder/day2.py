410ace251b5adcb29ff62f8d8836d56869ae485c
Computhink-CS146 <cs146@computhink.com.sg>
2025-12-02 06:38:25 +0000

    Dec 2, 2025, 6:38 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,21 +1,21 @@
-# Write all your codes for Day 2 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day2")
+# # Write all your codes for Day 2 here.
+# # COMMENT out the previous task before going on to the next task
+# print("hello from day2")
 
-########################################################################
-# Task 1:
-for count in range(100):
-    print("i will sling mud at my friends")
+# ########################################################################
+# # Task 1:
+# for count in range(100):
+#     print("i will sling mud at my friends")
 
-########################################################################
-# Task 2:
-name = "Jovan".upper()
-def cheer(name):
-    for letter in name:
-        print("give me a " + letter + "!")
-    print("who is the best??!?")
-    print(name + "!!!")
-cheer(name)
+# ########################################################################
+# # Task 2:
+# name = "Jovan".upper()
+# def cheer(name):
+#     for letter in name:
+#         print("give me a " + letter + "!")
+#     print("who is the best??!?")
+#     print(name + "!!!")
+# cheer(name)
 
 ########################################################################
 # Task 3:
