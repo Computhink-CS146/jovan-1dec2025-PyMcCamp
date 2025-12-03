8f8843585c015b4c18e6e722eab83119cbffe853
Computhink-CS146 <cs146@computhink.com.sg>
2025-12-03 08:52:38 +0000

    Dec 3, 2025, 8:52 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,50 +1,65 @@
-# # # # Write all your codes for Day 3 here.
-# # # # COMMENT out the previous task before going on to the next task
-# # # print("hello from day3")
+# # # # # # # # Write all your codes for Day 3 here.
+# # # # # # # # COMMENT out the previous task before going on to the next task
+# # # # # # # print("hello from day3")
 
-# # # ########################################################################
-# # # # Task 1:
-# # # name = input("what is your name?")
-# # # title = input("what is your title?")
-# # # command = input("what is your command?")
-# # # print(title + ", " + name + " commands the peasants to " + command)
-# # # ########################################################################
-# # # Task 2:
-# # name = input("what is your name?")
-# # num_pens = input("enter a number")
-# # print(name + " brought " + str(num_pens) + " pens.")
+# # # # # # # ########################################################################
+# # # # # # # # Task 1:
+# # # # # # # name = input("what is your name?")
+# # # # # # # title = input("what is your title?")
+# # # # # # # command = input("what is your command?")
+# # # # # # # print(title + ", " + name + " commands the peasants to " + command)
+# # # # # # # ########################################################################
+# # # # # # # Task 2:
+# # # # # # name = input("what is your name?")
+# # # # # # num_pens = input("enter a number")
+# # # # # # print(name + " brought " + str(num_pens) + " pens.")
 
+# # # # # # ########################################################################
+# # # # # # # Task 3:
+# # # # # num1 = int(input("Give me a number"))
+# # # # # num2 = int(input("Give me another number"))
+# # # # # num3 = num1 ** num2
+# # # # # print(str(num1) + " to the power of " + str(num2) + " is " + str(num3))
+# # # # # ########################################################################
+# # # # # # Task 4:
+# # # # price = int(999)
+# # # # num1 = int(input("How many items are you buying?"))
+# # # # num2 = num1 * price
+# # # # print("The total cost is $" + num2)
+# # # ########################################################################
+# # # # Task 5:
+# # # age1 = input("enter a number")
+# # # age2 = input("enter another number")
+# # # if age1 > age2:
+# # #     print(age1 + " is larger than " + age2)
+# # # elif age1 == age2:
+# # #     print(age1 + " and " + age2 + " are equal" )
+# # # else:
+# # #     print(age2 + " is larger than " + age1)
 # # ########################################################################
-# # # Task 3:
-# num1 = int(input("Give me a number"))
-# num2 = int(input("Give me another number"))
-# num3 = num1 ** num2
-# print(str(num1) + " to the power of " + str(num2) + " is " + str(num3))
+# # # Task 6:
+# # password = "Nope"
+# # entered_password = input("enter the password: ")
+# # if password == entered_password:
+# #     print("Access Granted!")
+# # else:
+# #     print("Access Denied.")
+# # ########################################################################
+# # # Task 7:
+# import random
+# for i in range(10):
+#     a = random.randint(-999, 999)
+#     print(a)
 # ########################################################################
-# # Task 4:
-price = int(999)
-num1 = int(input("How many items are you buying?"))
-num2 = num1 * price
-print("The total cost is $" + num2)
+# # Task 8:
+import random
+a = random.randint(-999, 999)
+b = random.randint(-999, 999)
+c = a + b
+d = input(a + "+" + b + "=")
+if d == c:
+    print("Sure. You, unfortunately, passed.")
+else:
+    print("Go see the principal, you failure.")
 ########################################################################
-# Task 5:
-
-
-
-########################################################################
-# Task 6:
-
-
-
-########################################################################
-# Task 7:
-
-
-
-########################################################################
-# Task 8:
-
-
-
-########################################################################
-# Additional exercises:
+# Additional execises
--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,75 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def forward(steps):
+    agent.move(FORWARD, steps)
+def back(steps):
+    agent.move(BACK, steps)
+def up(steps):
+    agent.move(UP, steps)
+def down(steps):
+    agent.move(DOWN, steps)
+def left():
+    agent.turn(LEFT)
+    # agent.move(LEFT, 1)
+def right():
+    agent.turn(RIGHT)
+    # agent.move(RIGHT, 1)
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("mf1", forward)
+player.on_chat("mb1", back)
+player.on_chat("mu1", up)
+player.on_chat("md1", down)
+player.on_chat("tl", left)
+player.on_chat("tr", right)
+player.on_chat("come", teleport)
+
+
+def obby1():
+    agent.move(FORWARD, 4)
+    agent.move(LEFT, 4)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 2)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+
+player.on_chat("obby", obby1)
+
+def chop(height):
+    for cut in range(height):
+        agent.destroy(FORWARD)
+        agent.move(UP, 1)
+    agent.move(DOWN, height)
+    agent.collect_all()
+player.on_chat("chop", chop)
+
+def mine(length):
+    agent.destroy(DOWN)
+    agent.move(DOWN, 1)
+    for i in range(length):
+        agent.destroy(FORWARD)
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+    agent.teleport_to_player()
+    
+player.on_chat("mine", mine)
+
+def buildwall(height, length):
+    for i in range(length):
+        for i in range(height):
+            agent.place(FORWARD)       
+            agent.move(UP,1)
+        agent.move(DOWN, height)
+        agent.move(RIGHT,1)
+player.on_chat("build", buildwall)
