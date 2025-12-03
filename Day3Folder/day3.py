ce7c7d62a6fef76d5d67bd04307afe51f74f0ca5
Computhink-CS146 <cs146@computhink.com.sg>
2025-12-03 07:22:46 +0000

    u

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,27 +1,31 @@
-# Write all your codes for Day 3 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# # # # Write all your codes for Day 3 here.
+# # # # COMMENT out the previous task before going on to the next task
+# # # print("hello from day3")
 
-########################################################################
-# Task 1:
+# # # ########################################################################
+# # # # Task 1:
+# # # name = input("what is your name?")
+# # # title = input("what is your title?")
+# # # command = input("what is your command?")
+# # # print(title + ", " + name + " commands the peasants to " + command)
+# # # ########################################################################
+# # # Task 2:
+# # name = input("what is your name?")
+# # num_pens = input("enter a number")
+# # print(name + " brought " + str(num_pens) + " pens.")
 
-
-
-########################################################################
-# Task 2:
-
-
-
-########################################################################
-# Task 3:
-
-
-
-########################################################################
-# Task 4:
-
-
-
+# # ########################################################################
+# # # Task 3:
+# num1 = int(input("Give me a number"))
+# num2 = int(input("Give me another number"))
+# num3 = num1 ** num2
+# print(str(num1) + " to the power of " + str(num2) + " is " + str(num3))
+# ########################################################################
+# # Task 4:
+price = int(999)
+num1 = int(input("How many items are you buying?"))
+num2 = num1 * price
+print("The total cost is $" + num2)
 ########################################################################
 # Task 5:
 
