12eda6dd06d9723bf9afe13ce1621e42cbfb60cf
Computhink-CS146 <cs146@computhink.com.sg>
2025-12-04 08:56:08 +0000

    Dec 4, 2025, 8:56 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -1,16 +1,32 @@
-# Write all your codes for Day 4 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day4")
+# # # # # Write all your codes for Day 4 here.
+# # # # # COMMENT out the previous task before going on to the next task
+# # # # print("hello from day4")
 
-########################################################################
-# Task 1:
+# # # # ########################################################################
+# # # # # Task 1:
+# # # num1 = 0
+# # # while num1 <= 9:
+# # #     print(num1)
+# # #     num1 += 1
+# # ##########################################################################
+# # num1 = 5
+# # while num1 <= 32:
+# #     print(num1)
+# #     num1 += 1
+# ##############################################################################
+# num1 = 50
+# while num1 >= 1:
+#     print(num1)
+#     num1 -= 1
+# ########################################################################
+# # Task 2:
+answer = input("I arrive before I’m sent. I’m received before I’m read. I can be lost even when I never existed. You create me, but you may not remember doing so. What am I?")
+realAnswer = "thought"
+while answer != realAnswer:
+    print("NOPE. try again.")
+    answer = input("I arrive before I’m sent. I’m received before I’m read. I can be lost even when I never existed. You create me, but you may not remember doing so. What am I?")
+else:
+    print("You have guessed correctly")
 
-
-
-########################################################################
-# Task 2:
-
-
-
-########################################################################
-# Additional exercises:
+# ########################################################################
+# # Additional exercises:
